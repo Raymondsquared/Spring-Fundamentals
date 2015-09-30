bd2de93eac34be02e474a3cb02c1435d32b8f013
Raymond <Raymond_Squared@live.com>
2015-09-30 22:20:41 +1000

    Setter Injection

--- spring_sample_xml/src/Application.java
+++ spring_sample_xml/src/Application.java
@@ -1,5 +1,6 @@
 import com.pluralsight.service.CustomerService;
-import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Dev on 29/09/2015.
@@ -8,7 +9,11 @@ public class Application
 {
     public static void main(String[] args)
     {
-        CustomerService service = new CustomerServiceImpl();
+        //CustomerService service = new CustomerServiceImpl();
+
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        CustomerService service = appContext.getBean("customerService", CustomerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
--- spring_sample_xml/src/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_xml/src/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,15 +2,17 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
 
-    @Override
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public List<Customer> findAll()
     {
         return customerRepository.findAll();
