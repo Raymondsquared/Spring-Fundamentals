f1fd95d868ced4717b8bc8c45ffe84bfd5bb3075
Raymond <Raymond_Squared@live.com>
2015-10-11 16:41:54 +1100

    Autowired

--- spring_sample_xml_anno/src/com/pluralsight/service/CustomerServiceImpl.java
+++ spring_sample_xml_anno/src/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,7 +2,7 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,9 +10,28 @@ import java.util.List;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    private CustomerRepository customerRepository;
 
-    @Override
+    public CustomerServiceImpl()
+    {
+
+    }
+
+    //@Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository)
+    {
+        System.out.println("we are using construction injection");
+        this.customerRepository = customerRepository;
+    }
+
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("we are using setter injection");
+
+        this.customerRepository = customerRepository;
+    }
+
+
     public List<Customer> findAll()
     {
         return customerRepository.findAll();
