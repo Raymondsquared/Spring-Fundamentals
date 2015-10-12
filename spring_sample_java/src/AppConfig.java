21b6676b84468838147e459c02192925ebfd889b
Raymond <Raymond_Squared@live.com>
2015-10-12 23:43:37 +1100

    Configuration

--- spring_sample_java/src/AppConfig.java
+++ spring_sample_java/src/AppConfig.java
@@ -1,7 +1,19 @@
-/**
+import com.pluralsight.service.CustomerService;
+import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+/*
  * Created by Dev on 12/10/2015.
  */
+
+@Configuration
 public class AppConfig
 {
-    
+    @Bean(name = "customerService")
+    public CustomerService getCustomerService()
+    {
+        return new CustomerServiceImpl();
+    }
+
 }
